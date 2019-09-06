0c862f6ab6f9f7cd307c6e6f3767cd04bb34bf38
fkahriman <fkahriman@hotmail.com>
2019-09-06 21:05:55 +0300

    Revision.

--- inst/shinyapp/app.R
+++ inst/shinyapp/app.R
@@ -6,33 +6,8 @@
 #
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
-# By default, the directories in .libPaths() aren't writable on shinyapps.io, so
-# create a subdir where we'll install our package.
-if (!file.exists("R-lib")) {
-  dir.create("R-lib")
-}
-# Unfortunately, there's no way to get deployapp() to ignore this directory, so
-# make sure to remove it locally before you call deployapp(). This can be done
-# with:
-#   unlink("pkgInst/R-lib", recursive = TRUE)
-
-# You may also need to restart R before calling deployapp(), because calling
-# runApp() will modify your libpath (below), which can confuse deployapp().
-
-# Add ./R-lib/ to the libPaths
-.libPaths( c(normalizePath("R-lib/"), .libPaths()) )
-
-# Install the package if needed.
-if (!do.call(require, list("BAFR"))) {
-  install.packages("BAFR_0.1.tar.gz", repos = NULL, type = "source")
-}
-
-# Instead of `library(myPackage)`, we'll use do.call, to evade deployapp's
-# checks for packages installed locally from source.
-do.call(library, list("BAFR"))
-
+runGitHub("fkahriman/BAFR",subdir = 'inst/shinyapp')
 
 ## Veri Giris Modulu----------------------------------------------------------------
 csvFileInput <- function(id, label = "CSV file") {
