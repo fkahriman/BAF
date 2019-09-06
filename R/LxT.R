ddc5ecd71fe0c871482052ae6edad4e08467b4d7
fkahriman <fkahriman@hotmail.com>
2019-09-06 23:07:20 +0300

    Commit

--- R/LxT.R
+++ R/LxT.R
@@ -2,11 +2,9 @@
 #'
 #' @description Bu paket ıslah denemelerinin analizi için geliştirilmiştir.
 #'
-#' @param replication  Replication as factor
+#' @param replications  Replications as factor
 #' @param Lines lines  as factor
 #' @param Testers testers  as factor
-#' @param genotypes genotypes  as factor
-#' @param gclass gclass  as factor
 #' @param yvar y  as variable
 #' @param data A \code{data.frame}
 #' @return NULL
@@ -18,15 +16,14 @@
 #' LinexTest(data)
 #'
 #'
-#' @import plantbreeding
+#' @import agricolae
 #'
 #' @export
 #'
 #'
 LinexTest <- function(data) {
-  require(plantbreeding)
-  LinexTest <- plantbreeding::line.tester(dataframe = file1, yvar = "trait1",  genotypes = "genotypes",
-                                           replication = "replication",  Lines = "Lines", Testers = "Tester", gclass = "gclass" )
+  require(agricolae)
+  LinexTest <- agricolae::lineXtester(replications, lines, testers, y)
   return(LinexTest)
 }
 
